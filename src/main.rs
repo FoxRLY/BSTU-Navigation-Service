1f89f1e9af86533b83a0e187739ee4f39497e9d0
FoxRLY <nevik0016@gmail.com>
2023-03-20 20:46:10 +0500

    Добавлена входная точка сервиса

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,22 @@
-use actix_navigation_service::server;
-use actix_navigation_service::mongo_client;
-use actix_web;
+use actix_navigation_service::server::Server;
+use tokio;
 
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    Ok(())
+fn main() {
+    let classroom_data = std::fs::read_to_string("classrooms.json").expect("No classrooms.json");
+    let image_data = std::fs::read_to_string("images.json").expect("No images.json");
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            Server::builder()
+                .image_data(image_data)
+                .classroom_data(classroom_data)
+                .host("0.0.0.0".to_owned())
+                .port(8080)
+                .build()
+                .start()
+                .await
+                .unwrap();
+        })
 }
